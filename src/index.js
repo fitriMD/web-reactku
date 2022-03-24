f2a9f03f78b98b9ffc5ed5e6eeeadcba0a9e1d29
fitriMD <mutiara.devi027@gmail.com>
2022-03-24 23:32:49 +0700

    Praktikum Js 6 Redux

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import MainReducer from './reducers/MainReducer'
+import App from './App';
+//import registerServiceWorker from './registerServiceWorker';
 
-const Hello = () => {
-  return <p>hello</p>
-}
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f =>
+  f)(createStore)(MainReducer)
 
-// ReactDOM.render(<App />,document.getElementById('root'));
-ReactDOM.render(<Hello />,document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+  <App />
+</Provider>,
+  document.getElementById('root'));
 
-// ReactDOM.render(
-//  <React.StrictMode>
-//    <App />
-//  </React.StrictMode>,
-//  document.getElementById('root')
-//);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//registerServiceWorker();
